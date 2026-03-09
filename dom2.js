c2effe6407e98ba2ac2c8749c8cafa14da8f019d
AnshulSonkar98 <anshulsonkar2222@gamail.com>
2026-03-09 23:55:40 +0530

    initial

---
+++ dom2.js
@@ -0,0 +1,26 @@
+let button = document.getElementById("addBtn");
+let list = document.getElementById("list");
+
+button.addEventListener("click", function () {
+
+  let li = document.createElement("li");
+
+  li.textContent = "New Item";
+
+  list.appendChild(li);
+
+});
+
+
+
+
+// To add Dynamic content 
+// button.addEventListener("click", function () {
+
+//   let li = document.createElement("li");
+
+//   li.textContent = "Item " + Math.floor(Math.random() * 100);
+
+//   list.appendChild(li);
+
+// });

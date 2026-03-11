f630e073123dba1e8953ba8ea8197359a788702c
AnshulSonkar98 <anshulsonkar2222@gamail.com>
2026-03-12 00:13:02 +0530

    initial

--- events.js
+++ events.js
@@ -1,15 +1,29 @@
-let counter = 0;
+// let counter = 0;
 
-let counterText = document.getElementById("counter");
-let increaseBtn = document.getElementById("increase");
-let decreaseBtn = document.getElementById("decrease");
+// let counterText = document.getElementById("counter");
+// let increaseBtn = document.getElementById("increase");
+// let decreaseBtn = document.getElementById("decrease");
 
-increaseBtn.addEventListener("click", function () {
-  counter++;
-  counterText.textContent = counter;
+// increaseBtn.addEventListener("click", function () {
+//   counter++;
+//   counterText.textContent = counter;
+// });
+
+// decreaseBtn.addEventListener("click", function () {
+//   counter--;
+//   counterText.textContent = counter;
+// });
+
+
+
+
+let title = document.getElementById("title");
+let button = document.getElementById("toggleBtn");
+
+button.addEventListener("click", function () {
+  title.classList.toggle("highlight");
 });
 
-decreaseBtn.addEventListener("click", function () {
-  counter--;
-  counterText.textContent = counter;
-});
+
+
+
